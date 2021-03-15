07510cd930abfb9229dbfdce2d063423bd4b2ed5
Tu <74278541+tuantu4194@users.noreply.github.com>
2021-03-15 21:39:19 +0700

    Update bool.h

--- bool.h
+++ bool.h
@@ -6,14 +6,13 @@
 
 #define MAX_PARTS 20
 
-//Error codes
+//Error codes for return
 #define NO_ERR 0
-#define ERR_EMPTY_VECTOR 1
-#define ERR_BAD_STRING 2
-#define ERR_SMALL_SHIFT -4
-#define ERR_BAD_ARG -2
-#define ERR_NULL_ARG -3
-#define ERR_EMPTY_STR -1
+#define EMPTY_ERR 1
+#define BADSTR_ERR 2
+#define BAD_LEN_SFT -4
+#define BAD_ARGS -2
+#define NULL_ARG -3
 
 typedef struct BigBool
 {
@@ -24,16 +23,32 @@ typedef struct BigBool
 
 
 
- 
+ /*
+    Mini Functions needed 
+ */
+int re_bit(BigBool *BB, int pos) {   //return bit [pos]
+    return (BB->vector[pos / 8] >> (pos % 8) & 1);
+}
+
+size_t highest_bit(uint64_t num) { //return the number of bit for (uint64)num
+        size_t bytes = 0;
+        size_t bits = 0;
+        while ((num >> ((bytes + 1) * 8)) != 0)
+            bytes++;
+        while ((num >> (bytes * 8 + bits)) != 0)
+            bits++;
+        return bytes * 8 + bits;
+};
+
  /* Return:
         1) NO_ERR if all work properly
-        2) ERR_NULL_ARG if BB == NULL
+        2) NULL_ARG if BB == NULL
  */   
 int BB_reset(BigBool *BB); // set all bit to 0
 {
     if (BB == NULL) 
     {
-        return ERR_NULL_ARG;
+        return NULL_ARG;
     }
     memset(BB->vector, 0, MAX_PARTS);
     BB->num_bit = 0;
@@ -44,19 +59,19 @@ int BB_reset(BigBool *BB); // set all bit to 0
 
  
  /* Return:
-        1) NO_ERR if all work properly,
-        2) ERR_NULL_ARG if BB == NULL,
-        3) ERR_BAD_ARG if input number bad,
+        1) NO_ERR if all work,
+        2) NULL_ARG if BB == NULL,
+        3) BAD_ARGS if bad input(wrong format),
  */
 int BB_reverse_bits(BigBool *BB, int sBit, int eBit) // reverse BB from sBit to eBit
 {
     if (BB == NULL) 
     {
-        return ERR_NULL_ARG;
+        return NULL_ARG;
     }
     if ( sBit < 0 || eBit > MAX_PARTS*8 || sBit > eBit) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     BigBool tmp;
     if (eBit/8 > BB->num_part) // if amount part > than amount part BB, set part BB to new
@@ -66,7 +81,7 @@ int BB_reverse_bits(BigBool *BB, int sBit, int eBit) // reverse BB from sBit to 
     memcpy(&tmp, BB, MAX_PARTS); // BB save in tmp   
     for(int pos = sBit; pos < eBit; pos++) 
     {
-        tmp.vector[pos / 8] = tmp.vector[pos / 8] & ~(1 << (pos % 8));
+        tmp.vector[pos / 8] = tmp.vector[pos / 8] & ~(1 << (pos % 8)); //set bit [pos] to 0
         tmp.vector[pos / 8] |= get_bit(BB, (eBit - pos + sBit - 1)) << (pos % 8);
     }
     memcpy(BB, &tmp, MAX_PARTS);
@@ -76,23 +91,23 @@ int BB_reverse_bits(BigBool *BB, int sBit, int eBit) // reverse BB from sBit to 
 /* 
    
  Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_NULL_ARG if BB == NULL,
-        3) ERR_EMPTY_VECTOR if strlen(str) == 0,
-        4) ERR_BAD_STRING if string is not only '1' and '0'.
+        1) NO_ERR if function work,
+        2) NULL_ARG if BB == NULL,
+        3) EMPTY_ERR if strlen(str) == 0,
+        4) BADSTR_ERR if string is not only '1' and '0'.
  */
-int BB_from_str(char* str , BigBool* res ) // convert string with char '0' and '1' to vecto bool
+int BB_from_str(char* str , BigBool* res ) // convert string with only '0' and '1' to vecto bool
 {
 
     if (res == NULL) 
     {
-        return ERR_NULL_ARG;
+        return NULL_ARG;
     }
 
     size_t len = strlen(str);
     if(len == 0) 
     {
-        return ERR_EMPTY_VECTOR;
+        return EMPTY_ERR;
     }
     memset(res->vector, 0, MAX_PARTS);
     size_t num_part = len / 8;
@@ -110,7 +125,7 @@ int BB_from_str(char* str , BigBool* res ) // convert string with char '0' and '
             index = len - 1 - (part * 8 + bit);
             if(str[index] != '0' && str[index] != '1') 
             {
-                return ERR_BAD_STRING;
+                return BADSTR_ERR;
             }
             res->vector[part] |= ((str[index] - '0') << bit);
         }
@@ -121,7 +136,7 @@ int BB_from_str(char* str , BigBool* res ) // convert string with char '0' and '
         index = len - 1 - (num_part * 8 + bit);
         if (str[index] != '0' && str[index] != '1') 
         {
-            return ERR_BAD_STRING;
+            return BADSTR_ERR;
         }
         res->vector[num_part] |= ((str[index] - '0') << bit);
 
@@ -129,10 +144,11 @@ int BB_from_str(char* str , BigBool* res ) // convert string with char '0' and '
     return NO_ERR;
 }
 
+
 /*
  Returns:
-        1) ERR_OK if function work properly,
-        2) ERR_NULL_ARG if res == NULL.
+        1) NO_ERR
+        2) NULL_ARG if res == NULL.
  */
 
 int BB_from_uint64(uint64_t num , BigBool* res) // Convert Unsigned Long Integer (64 bit) to big bool Vecter
@@ -140,7 +156,7 @@ int BB_from_uint64(uint64_t num , BigBool* res) // Convert Unsigned Long Integer
 
     if (res == NULL)
     {
-        return ERR_NULL_ARG;
+        return NULL_ARG;
     }
     memset(res->vector, 0, MAX_PARTS);
 
@@ -149,7 +165,7 @@ int BB_from_uint64(uint64_t num , BigBool* res) // Convert Unsigned Long Integer
         res->vector[i] |= (num >> 8 * i);
     }
 
-    int pos = high_bit_pos_cycle(num);
+    int pos = highest_bit(num);
     res->num_part = pos / 8;
     res->num_bit = pos % 8;
     return NO_ERR;
@@ -160,10 +176,10 @@ Returns:
         1) Pointer to string,
         2) NULL, if failed to allocate.
  */
-char* BB_to_str(BigBool* res) // covert a BB vecto to string '0' and '1'
+char* BB_to_str(BigBool* res) // convert a BB vecto to string 
 {
 
-    size_t len = len_bits(BB);
+    size_t len = res->num_part * 8 + res->num_bit;
 
     char* str = (char*)malloc(len*sizeof(char));
     if (str == NULL)
@@ -173,7 +189,7 @@ char* BB_to_str(BigBool* res) // covert a BB vecto to string '0' and '1'
 
     for (int strPos = 0; strPos < len; strPos++)
     {
-        str[len - strPos - 1] = '0' + get_bit(BB, strPos);
+        str[len - strPos - 1] = '0' + re_bit(BB, strPos);
     }
     str[len] = '\0';
 
@@ -182,15 +198,15 @@ char* BB_to_str(BigBool* res) // covert a BB vecto to string '0' and '1'
 
 /*
 Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL.
+        1) NO_ERR
+        2) BAD_ARGS if BB == NULL.
  */
-int BB_disjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 | BB2
+int BB_disjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 | BB2, result in res 
 {
 
     if (BB1 == NULL || res == NULL || BB2 == NULL)
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if(len_bits(BB1) < len_bits(BB2))
     {
@@ -214,15 +230,15 @@ int BB_disjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 | BB2
 
 
  /* Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL.
+        1) NO_ERR
+        2) BAD_ARGS if BB == NULL.
  */
-int BB_conjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 & BB2
+int BB_conjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 & BB2, result in res
 {
 
     if (BB1 == NULL || res == NULL || BB2 == NULL) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if(len_bits(BB1) < len_bits(BB2)) 
     {
@@ -246,14 +262,14 @@ int BB_conjunction (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 & BB2
 /*
 Returns:
         1) NO _ ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL.
+        2) BAD_ARGS if BB == NULL.
  */
-int BB_xor (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 XOR BB2
+int BB_xor (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 XOR BB2, return to res
 {
 
     if (BB1 == NULL || res == NULL || BB2 == NULL) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if(len_bits(BB1) < len_bits(BB2)) 
     {
@@ -276,15 +292,15 @@ int BB_xor (BigBool* BB1, BigBool* BB2, BigBool* res) // FIND BB1 XOR BB2
 
 /*
  * Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL.
+        1) NO_ERR,
+        2) BAD_ARGS if BB == NULL.
  */
 int BB_inversion (BigBool* BB1, BigBool* res) // makes inversion of bool vector ( ~BB ) 
 {
 
     if (BB1 == NULL || res == NULL) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     res->num_bit = BB1->num_bit;
     res->num_part = BB1->num_part;
@@ -299,16 +315,16 @@ int BB_inversion (BigBool* BB1, BigBool* res) // makes inversion of bool vector 
 
 /*
 Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL,
-        3) ERR_BIG_SHIFT if len of shift is bigger than len of vector,
-        4) ERR_SMALL_SHIFT if len of shift less than 0.
+        1) NO_ERR,
+        2) BAD_ARGS if BB == NULL,
+        3) ERR_BIG_SHIFT if num shift > len of vector,
+        4) BAD_LEN_SFT if num shift < 0.
  */
-int BB_left_shift (BigBool* BB, BigBool* res, int len_shift) // do left shift number input ( << integer )
+int BB_left_shift (BigBool* BB, BigBool* res, int len_shift) // do left shift number input ( << num )
 {
     if (BB == NULL || res == NULL || len_shift < 0) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if (len_shift >= MAX_PARTS*8) 
     {
@@ -348,16 +364,16 @@ int BB_left_shift (BigBool* BB, BigBool* res, int len_shift) // do left shift nu
 
 /*
 Returns:
-        1) ERR_OK if function work properly,
-        2) ERR_BAD_ARG if BB == NULL,
-        3) ERR_BIG_SHIFT if len of shift is bigger than len of vector,
-        4) ERR_SMALL_SHIFT if len of shift less than 0.
+        1) NO_ERR,
+        2) BAD_ARGS if BB == NULL,
+        3) ERR_BIG_SHIFT if num shift > len of vector,
+        4) BAD_LEN_SFT if num shift < 0.
  */
-int BB_right_shift (BigBool* BB, BigBool* res, int len_shift) // do RIGHT shift number input ( >> integer )
+int BB_right_shift (BigBool* BB, BigBool* res, int len_shift) // do RIGHT shift number input ( >> num )
 {
 
     if (BB == NULL || res == NULL || len_shift < 0) {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if (len_shift >= len_bits(BB)) {
         memset(res->vector, 0, MAX_PARTS);
@@ -389,20 +405,20 @@ int BB_right_shift (BigBool* BB, BigBool* res, int len_shift) // do RIGHT shift 
 
 /*
  Returns:
-        1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL,
-        3) BB_cyclic_right_shift(BB, res, -len_shift) if len of shift less than 0,
+        1) NO_ERR,
+        2) BAD_ARGS if BB == NULL,
+        3) BB_Cycle_Right(BB, res, -len_shift) if num shift < 0,
 
  */
-int BB_cyclic_left_shift (BigBool* BB, BigBool* res, int len_shift) // do cyclic left shift of bool vector
+int BB_Cycle_Left (BigBool* BB, BigBool* res, int len_shift) // do cyclic left shift of bool vector
 {
     if (BB == NULL || res == NULL) 
     {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     if (len_shift < 0) 
     {
-        return BB_cyclic_right_shift(BB, res, -len_shift);
+        return BB_Cycle_Right(BB, res, -len_shift);
     }
     len_shift %= len_bits(BB);
     memcpy(res, BB, MAX_PARTS);
@@ -417,14 +433,14 @@ int BB_cyclic_left_shift (BigBool* BB, BigBool* res, int len_shift) // do cyclic
 /*
  * Returns:
         1) NO_ERR if function work properly,
-        2) ERR_BAD_ARG if BB == NULL,
-        3) BB_cyclic_left_shift(BB, res, -len_shift) if len of shift less than 0,
+        2) BAD_ARGS if BB == NULL,
+        3) BB_Cycle_Left(BB, res, -len_shift) if num shift < 0,
  *
  */
-int BB_cyclic_right_shift (BigBool*BB, BigBool* res, int len_shift) // do cyclic right shift of bool vector
+int BB_Cycle_Right (BigBool*BB, BigBool* res, int len_shift) // do cyclic right shift of bool vector
 {
     if (BB == NULL || res == NULL) {
-        return ERR_BAD_ARG;
+        return BAD_ARGS;
     }
     int shift_amount;
     if (len_shift < 0) {
@@ -433,7 +449,7 @@ int BB_cyclic_right_shift (BigBool*BB, BigBool* res, int len_shift) // do cyclic
     else {
         shift_amount = len_bits(BB) - len_shift;
     }
-    return BB_cyclic_left_shift(BB, res, shift_amount);
+    return BB_Cycle_Left(BB, res, shift_amount);
 }
 
 #endif //PROGS_LIBRARY_H
